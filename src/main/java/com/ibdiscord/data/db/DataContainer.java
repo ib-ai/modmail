cad0d36ac392c4abcdc2f07a1818e1cc640bc383
Ray Clark <raynichc@users.noreply.github.com>
2020-12-03 19:35:43 +1100

    Added Copyright, Javadocs, fixed style and added pool size and leak check

--- src/main/java/com/ibdiscord/data/db/DataContainer.java
+++ src/main/java/com/ibdiscord/data/db/DataContainer.java
@@ -1,3 +1,22 @@
+/* Copyright 2020 Ray Clark <raynichclark@gmail.com>
+ *
+ * This file is part of Modmail.
+ *
+ * Modmail is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Modmail is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Modmail. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
 package com.ibdiscord.data.db;
 
 import com.ibdiscord.Modmail;
@@ -10,13 +29,20 @@ import java.sql.SQLException;
 
 public enum DataContainer {
 
+    /**
+     * Singleton instance of container.
+     */
     INSTANCE;
 
     private HikariDataSource ds;
 
+    /**
+     * Connect to the database.
+     */
     public void connect() {
-        if (ds != null)
+        if (ds != null) {
             return;
+        }
 
         LocalConfig config = Modmail.INSTANCE.getConfig();
 
@@ -25,12 +51,30 @@ public enum DataContainer {
         dbConfig.setUsername(config.getDbUsername());
         dbConfig.setPassword(config.getDbPassword());
 
+        dbConfig.setMaximumPoolSize(30);
+        dbConfig.setLeakDetectionThreshold(2500);
+
         ds = new HikariDataSource(dbConfig);
     }
 
+    /**
+     * Close the database connection pool.
+     */
+    public void close() {
+        if (ds != null) {
+            ds.close();
+        }
+    }
+
+    /**
+     * Get a Database Connection.
+     * @return Connection
+     * @throws SQLException SQLException
+     */
     public Connection getConnection() throws SQLException {
-        if (ds == null)
+        if (ds == null) {
             connect();
+        }
 
         return ds.getConnection();
     }
