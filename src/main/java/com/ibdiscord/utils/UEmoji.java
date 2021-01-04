b02bc47b452279fd346f2ba5629865b648de5f17
Ray Clark <raynichc@users.noreply.github.com>
2021-01-04 18:38:33 +1100

    Added Emojis

---
+++ src/main/java/com/ibdiscord/utils/UEmoji.java
@@ -0,0 +1,33 @@
+/* Copyright 2021 Ray Clark <raynichclark@gmail.com>
+ *
+ * This file is part of Modmail.
+ *
+ * Modmail is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Modmail is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Modmail. If not, see http://www.gnu.org/licenses/.
+ *
+ */
+
+package com.ibdiscord.utils;
+
+public class UEmoji {
+
+    // Ticket emojis
+    public static final String CLOSE_TICKET_EMOJI = "U+274E";
+    public static final String REPLY_TICKET_EMOJI = "U+1F5E3";
+    public static final String TIMEOUT_TICKET_EMOJI = "U+23F2";
+
+    // Confirmation emojis
+    public static final String YES_CONFIRMATION_EMOJI = "U+2705";
+    public static final String NO_CONFIRMATION_EMOJI = "U+274E";
+
+}
