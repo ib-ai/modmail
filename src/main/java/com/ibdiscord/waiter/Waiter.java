03104901aaec4eaab06538ef7082ffa7f133778d
Ray Clark <raynichc@users.noreply.github.com>
2021-01-05 18:31:17 +1100

    Updated TODOs

--- src/main/java/com/ibdiscord/waiter/Waiter.java
+++ src/main/java/com/ibdiscord/waiter/Waiter.java
@@ -76,7 +76,7 @@ public enum Waiter {
     public void input(Member member, String input) {
         WaitTask task = tasks.get(member);
         if (task == null) {
-            //TODO: Log
+            //TODO: Log null task
             return;
         }
         if (task.getHandler().onInput(input)) {
@@ -92,7 +92,7 @@ public enum Waiter {
     public void react(Member member, String emoji) {
         WaitTask task = tasks.get(member);
         if (task == null) {
-            //TODO: Log
+            //TODO: Log null task
             return;
         }
         if (task.getHandler().onReact(emoji)) {
@@ -107,7 +107,7 @@ public enum Waiter {
     public void cancel(Member member) {
         WaitTask task = tasks.get(member);
         if (task == null)   {
-            //TODO: Log
+            //TODO: Log null task
             return;
         }
         handleEnd(member, task, true);
