209bdcc712ca190c63629192bef21ef039c22b6b
Ray Clark <raynichc@users.noreply.github.com>
2021-01-05 15:44:24 +1100

    Implemented server message listener

--- src/main/java/com/ibdiscord/listeners/MessageListener.java
+++ src/main/java/com/ibdiscord/listeners/MessageListener.java
@@ -21,7 +21,16 @@ package com.ibdiscord.listeners;
 
 import com.ibdiscord.Modmail;
 import com.ibdiscord.data.db.DataContainer;
+import com.ibdiscord.utils.UChannel;
+import com.ibdiscord.utils.UEmoji;
+import com.ibdiscord.utils.UFormatter;
+import com.ibdiscord.utils.objects.Ticket;
+import com.ibdiscord.utils.objects.TicketResponse;
 import com.ibdiscord.waiter.Waiter;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
+import net.dv8tion.jda.api.entities.TextChannel;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -33,12 +42,17 @@ import java.sql.SQLException;
 import java.sql.Timestamp;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class MessageListener extends ListenerAdapter {
 
     @Override
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-        if (event.getChannel().getId() != Modmail.INSTANCE.getConfig().getChannelId()) {
+        if (event.getAuthor().isBot()) {
+            return;
+        }
+
+        if (!Objects.equals(event.getChannel().getId(), Modmail.INSTANCE.getConfig().getChannelId())) {
             return;
         }
 
@@ -47,6 +61,10 @@ public class MessageListener extends ListenerAdapter {
 
     @Override
     public void onPrivateMessageReceived(PrivateMessageReceivedEvent event) {
+        if (event.getAuthor().isBot()) {
+            return;
+        }
+
         try (Connection con = DataContainer.INSTANCE.getConnection()) {
             long userID = event.getAuthor().getIdLong();
             PreparedStatement pst = con.prepareStatement("SELECT \"ticket_id\", \"timeout\" FROM \"mm_tickets\" WHERE \"user\"=? AND \"open\"=TRUE;");
@@ -75,7 +93,6 @@ public class MessageListener extends ListenerAdapter {
             }
 
             int ticketId = result.getInt("ticket_id");
-            System.out.println(ticketId);
 
             pst = con.prepareStatement("INSERT INTO \"mm_ticket_responses\" (\"ticket_id\", \"user\", \"response\", \"as_server\")"
                     + "VALUES (?, ?, ?, FALSE)"
@@ -86,6 +103,37 @@ public class MessageListener extends ListenerAdapter {
             pst.execute();
 
             event.getMessage().addReaction("U+1F4E8").queue();
+
+            Guild guild = event.getJDA().getGuildById(Modmail.INSTANCE.getConfig().getGuildID());
+
+            Ticket ticket = new Ticket(guild.getMember(event.getAuthor()));
+            pst = con.prepareStatement("SELECT \"user\", \"response\", \"timestamp\" FROM \"mm_ticket_responses\" WHERE \"ticket_id\"=? ORDER BY \"response_id\" ASC");
+            pst.setLong(1, ticketId);
+            ResultSet results = pst.executeQuery();
+            while (results.next()) {
+                ticket.addResponse(new TicketResponse(guild.getMemberById(results.getLong("user")),
+                        results.getString("response"),
+                        results.getTimestamp("timestamp")));
+            }
+            MessageEmbed ticketEmbed = UFormatter.ticketEmbed(ticket);
+
+            //TODO: remove old message, if exists
+            TextChannel modmailChannel = UChannel.getModmailChannel(guild);
+            if (modmailChannel != null) {
+                Message message = modmailChannel.sendMessage(ticketEmbed).complete();
+                if (message != null) {
+                    message.addReaction(UEmoji.REPLY_TICKET_EMOJI).queue();
+                    message.addReaction(UEmoji.CLOSE_TICKET_EMOJI).queue();
+                    message.addReaction(UEmoji.TIMEOUT_TICKET_EMOJI).queue();
+                    pst = con.prepareStatement("UPDATE \"mm_tickets\" SET \"message_id\"=? WHERE \"ticket_id\"=?");
+                    pst.setLong(1, message.getIdLong());
+                    pst.setLong(2, ticketId);
+                    if (pst.executeUpdate() > 0) {
+                        //TODO:
+                    }
+                }
+            }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
